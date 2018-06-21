632b6d87385f662dec1b0b2fcb2ad01f34c01f4f
Daria Bogdan <darya.alex@list.ru>
2018-06-21 13:40:51 +0300

    added comments

--- hadoop_task_1/test.py
+++ hadoop_task_1/test.py
@@ -3,7 +3,8 @@ import re
 
 
 WORD_RE = re.compile(r"[\w']+")
-PARAM = 30
+PARAM = 30  # amount of the longest words to be shown
+
 
 class MRMostUsedWord(MRJob):
 
@@ -21,34 +22,41 @@ class MRMostUsedWord(MRJob):
         if self.yielded_mapper == PARAM:
             return
 
-        d = {}
+        # creating a dict to aggregate words by its length
+        words_by_len = {}
         for word in WORD_RE.findall(line):
-            d[len(word)] = d.get(len(word), set())
-            d[len(word)].add(word.lower())
-        if len(d):
-            max_len = max(d.keys())
+            words_by_len[len(word)] = words_by_len.get(len(word), set())
+            words_by_len[len(word)].add(word.lower())
 
-            for i in range(min(PARAM, len(d[max_len]))):
+        if len(words_by_len):  # yielding only if string is not empty
+            max_len = max(words_by_len.keys())
+
+            # yielding only needed amount of words
+            for _ in range(min(PARAM, len(words_by_len[max_len]))):
                 self.yielded_mapper += 1
-                yield (max_len, d[max_len].pop())
+                yield (max_len, words_by_len[max_len].pop())
 
     def combiner_init(self):
         self.yielded_combiner = 0
 
     def combiner(self, length, words):
+        # words comes from different blocks, so they can be repeated
+        # creating set to save only unique words
         words = set(words)
         while words and self.yielded_combiner != PARAM:
-            self.yielded_combiner +=1
+            self.yielded_combiner += 1
             yield (length, words.pop())
 
     def reducer_init(self):
         self.yielded_reducer = 0
 
     def reducer(self, length, words):
+        # creating set to save only unique words
         words = set(words)
         while words and self.yielded_reducer != PARAM:
             self.yielded_reducer += 1
             yield None, (length, words.pop())
+
 
 if __name__ == '__main__':
     MRMostUsedWord.run()
