db89932a0364a417f41c4aa5f66f525c01e0280c
Daria Bogdan <darya.alex@list.ru>
2018-06-24 18:46:19 +0300

    comments added

--- hadoop_task_1/longest_words.py
+++ hadoop_task_1/longest_words.py
@@ -1,12 +1,26 @@
+"""
+Finds several longest words.
+Amount of words to be found are specified with --top argument
+"""
+
 from mrjob.job import MRJob
 import re
 import mrjob
 
-
+# regular expression to find words in line
 WORD_RE = re.compile(r"[\w']+")
 
 
 class MRLongestWord(MRJob):
+    """Map Reduce Job that finds several longest words.
+
+    Attributes:
+        yielded_combiner: amount of already yielded words.
+        yielded_words_combiner: already yielded words.
+        yielded_reducer: amount of already yielded words.
+        yielded_words_reducer: already yielded words.
+
+    """
 
     OUTPUT_PROTOCOL = mrjob.protocol.RawValueProtocol
 
@@ -18,6 +32,7 @@ class MRLongestWord(MRJob):
     }
 
     def configure_options(self):
+        # add --top argument
         super().configure_options()
         self.add_passthrough_option(
             '--top',
@@ -33,25 +48,29 @@ class MRLongestWord(MRJob):
 
     def combiner_init(self):
         self.yielded_combiner = 0
-        self.yielded_words_combiner = set()  # to show only unique words
+        self.yielded_words_combiner = []
 
     def combiner(self, length, words):
+        # yield only needed amount of words
         while words and self.yielded_combiner != self.options.top:
             next_word = next(words)
+            # yield only if this word was not yielded yet
             if next_word not in self.yielded_words_combiner:
-                self.yielded_words_combiner.add(next_word)
+                self.yielded_words_combiner.append(next_word)
                 self.yielded_combiner += 1
                 yield length, next_word
 
     def reducer_init(self):
         self.yielded_reducer = 0
-        self.yielded_words_reducer = set() # to show only unique words
+        self.yielded_words_reducer = []
 
     def reducer(self, length, words):
+        # yield only needed amount of words
         while words and self.yielded_reducer != self.options.top:
             next_word = next(words)
+            # yield only if this word was not yielded yet
             if next_word not in self.yielded_words_reducer:
-                self.yielded_words_reducer.add(next_word)
+                self.yielded_words_reducer.append(next_word)
                 self.yielded_reducer += 1
                 yield length, next_word
 
