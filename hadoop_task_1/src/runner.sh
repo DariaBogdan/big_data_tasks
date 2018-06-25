482397e327d48430330923020b36414a29161168
Daria Bogdan <darya.alex@list.ru>
2018-06-26 01:17:17 +0300

    added logfiles

--- hadoop_task_1/src/runner.sh
+++ hadoop_task_1/src/runner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.4 test.py -r hadoop hdfs:///user/raj_ops/war_and_peace.txt hdfs:///user/raj_ops/jerom.txt --hadoop-streaming-jar=/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar --python-bin=python3.4 --top=4
+python3.4 longest_words.py -r hadoop hdfs:///user/raj_ops/war_and_peace.txt hdfs:///user/raj_ops/jerom.txt --hadoop-streaming-jar=/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar --python-bin=python3.4 --top=30
