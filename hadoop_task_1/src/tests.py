c5d30ef305de5f9ffbe0062344e3689d3c598417
Daria Bogdan <darya.alex@list.ru>
2018-06-27 15:16:00 +0300

    requirements changed

--- hadoop_task_1/src/tests.py
+++ hadoop_task_1/src/tests.py
@@ -7,8 +7,9 @@ from longest_words import MRLongestWord
 class TestMR(unittest.TestCase):
 
     def test_mapper_standard_input_line(self):
+        job = MRLongestWord(args=['--top=2'])
         input = 'The story begins in the 1978. They met at school! Why?'
-        res = MRLongestWord.mapper(None, None, input)
+        res = job.mapper(None, input)
 
         expected_output = [(3, 'the'), (5, 'story'), (6, 'begins'), (2, 'in'), (3, 'the'), (4, '1978'), (4, 'they'),
                            (3, 'met'), (2, 'at'), (6, 'school'), (3, 'why')]
@@ -16,16 +17,18 @@ class TestMR(unittest.TestCase):
         self.assertIsInstance(res, types.GeneratorType)
 
     def test_mapper_empty_input_line(self):
+        job = MRLongestWord(args=['--top=2'])
         input = ''
-        res = MRLongestWord.mapper(None, None, input)
+        res = job.mapper(None, input)
 
         expected_output = []
         self.assertEqual(list(res), expected_output)
         self.assertIsInstance(res, types.GeneratorType)
 
     def test_mapper_input_line_without_words(self):
+        job = MRLongestWord(args=['--top=2'])
         input = '!&&)((@&$*(@(@)(#**$&#$^@%%^@!&@^'
-        res = MRLongestWord.mapper(None, None, input)
+        res = job.mapper(None, input)
 
         expected_output = []
         self.assertEqual(list(res), expected_output)
