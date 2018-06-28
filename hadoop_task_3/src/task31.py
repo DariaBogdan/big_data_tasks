f98140d6334552b36f09ddc0c032f6c2f92cb777
Daria Bogdan <darya.alex@list.ru>
2018-06-28 09:53:32 +0300

    regexp added

--- hadoop_task_3/src/task31.py
+++ hadoop_task_3/src/task31.py
@@ -1,5 +1,30 @@
 from mrjob.job import MRJob
+import re
 
+regex = re.compile('([a-zA-Z0-9]+)\t'  # BidID -- 0
+                   '([0-9]+)\t'  # Timestamp -- 1
+                   '(\d)+\t'  # LogType -- 2
+                   '([a-zA-Z0-9]+)\t'  # iPinYouID -- 3
+                   '(.+)\t'  #User-Agent -- 4
+                   '([0-9.*]*)\t'  #IP -- 5
+                   '(\d+)\t'  # RegionID -- 6
+                   '(\d+)\t'  # CityID -- 7
+                   '(\d)+\t'  # AdExchange -- 8
+                   '([a-zA-Z0-9]+)\t'  # Domain -- 9
+                   '([a-zA-Z0-9]+)\t'  # URl -- 10
+                   '(null)\t'  # Anonymous URL -- 11
+                   '([0-9_a-zA-Z]+)\t'  # Ad Slot ID -- 12
+                   '(\d+)\t'  # Ad Slot Width -- 13
+                   '(\d+)\t'  # Ad Slot Heigth -- 14
+                   '([a-zA-Z]+)\t'  # Ad Slot Visibility -- 15
+                   '(Na)\t'  # Ad Slot Format -- 16
+                   '(\d+)\t'  # Ad Slot Floor Price -- 17
+                   '([0-9a-zA-Z]+)\t'  # Creative ID -- 18
+                   '(\d+)\t'  # Bidding Price -- 19
+                   '(\d+)\t'  # Paying Price -- 20
+                   '(null)\t'  # Landing Page URL -- 21
+                   '(\d+)\t'  # Advertiser ID -- 22
+                   '(.+)')  # User Profile IDs -- 23
 PRICE_LIMIT = 250
 
 class MRCity(MRJob):
@@ -9,11 +34,13 @@ class MRCity(MRJob):
         self.add_file_arg('--city_names_file')
 
     def mapper(self, _, line):
-        res = line.split('\t')
-        city_id = res[7]
-        b_id = int(res[19])
-        if b_id > PRICE_LIMIT:
-            yield city_id, 1
+        matches = re.match(regex, line)
+        if matches:
+            groups = matches.groups()
+            city_id = groups[7]
+            bid_price = int(groups[19])
+            if bid_price > PRICE_LIMIT:
+                yield city_id, 1
 
     def combiner(self, key, values):
         yield key, sum(values)
