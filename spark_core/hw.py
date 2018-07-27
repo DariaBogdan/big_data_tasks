4be892ead61d80d89a72d7baacd337b79fc58598
Daria Bogdan <darya.alex@list.ru>
2018-07-27 18:22:57 +0300

    changed

--- spark_core/hw.py
+++ spark_core/hw.py
@@ -7,74 +7,74 @@ class MotelsHomeRecommendation:
     ERRONEOUS_DIR = "erroneous"
     AGGREGATED_DIR = "aggregated"
 
-    def getRawBids(sc, bidsPath):
+    def __init__(self, bidsPath, exchangeRatesPath, motelsPath, outputBasePath):
+        self.bidsPath = bidsPath
+        self.exchangeRatesPath = exchangeRatesPath
+        self.motelsPath = motelsPath
+        self.outputBasePath = outputBasePath
+        self.sc = SparkContext.getOrCreate(SparkConf())
+        self.spark = SparkSession(self.sc)
+
+    def getRawBids(self, sc, bidsPath):
         text_file = sc.textFile(bidsPath)
         return text_file.map(lambda row: row)
 
-    def getErroneousRecords(rawBids):
+    def getErroneousRecords(self, rawBids):
         rdd = rawBids.map(lambda r: r) \
             .filter(lambda r: r.find('ERROR') > 0) \
-            .map(lambda x: (','.join(x.split(',')[1:]), 1)) \
+            .map(lambda x: x.split(',')) \
+            .map(lambda x: Row(motelId=x[0], date=x[1], error=x[2])) \
+            .map(lambda x: ((x.date, x.error), 1)) \
             .reduceByKey(lambda a, b: a + b)
-        rdd.saveAsTextFile('rdd9')
         return rdd
 
-    def getExchangeRates(sc, exchangeRatesPath):
+    def getExchangeRates(self, sc, exchangeRatesPath):
         text_file = sc.textFile(exchangeRatesPath)
         return text_file.map(lambda r: [r.split(',')[i] for i in [0, 3]])
 
-    def getBids(rawBids, exchangeRates):
+    def getBids(self, rawBids, exchangeRates):
         w = ['US', 'MX', 'CA']
         rdd = rawBids \
             .filter(lambda r: not "ERROR" in r) \
             .map(lambda r: [r.split(',')[i] for i in [0, 1, 5, 6, 8]]) \
-            .flatMap(lambda x: [(x[1], x[0], x[i + 2], w[i]) for i in range(3)]) \
-            .filter(lambda x: x[2] != '') \
-            .map(lambda x: (x[0], x[1:])) \
+            .flatMap(lambda x: [Row(date=x[1], motelId=x[0], bid=x[i + 2], country=w[i]) for i in range(3)]) \
+            .filter(lambda x: x.bid != '') \
+            .map(lambda x: (x.date, (x.motelId, x.bid, x.country))) \
             .leftOuterJoin(exchangeRates) \
-            .map(lambda x: (x[1][0][0], (x[0], round(float(x[1][0][1]) * float(x[1][1]), 3), x[1][0][2])))
+            .map(lambda x: Row(date=x[0], motelId=x[1][0][0], bid=x[1][0][1], country=x[1][0][2], ex_rate=x[1][1])) \
+            .map(lambda x: (x.motelId, (x.date, round(float(x.bid) * float(x.ex_rate), 3), x.country)))
         return rdd
 
-    def getMotels(sc, motelsPath):
+    def getMotels(self, sc, motelsPath):
         text_file = sc.textFile(motelsPath)
         return text_file.map(lambda r: [r.split(',')[i] for i in [0, 1]])
 
-    def getEnriched(bids, motels):
+    def getEnriched(self, bids, motels):
         rdd = bids.leftOuterJoin(motels) \
-            .map(lambda x: ((x[0], x[1][0]), (x[1][1], x[1][2]))) \
-            .reduceByKey(lambda x, y: max(x, y, key=lambda x: x[0]))
+            .map(lambda x: (Row(motelId=x[0], date=x[1][0][0]), Row(bid=x[1][0][1], country=x[1][0][2], name=x[1][1]))) \
+            .reduceByKey(lambda x, y: max(x, y, key=lambda y: y.bid)) \
+            .map(lambda x: (x[0].motelId, x[1].name, x[0].date, x[1].country, x[1].bid))
         return rdd
 
-    def processData(self, sc, bidsPath, motelsPath, exchangeRatesPath, outputBasePath):
-
+    def processData(self):
         # Read the bid data from the provided file.
-        rawBids = self.getRawBids(sc, bidsPath)
+        rawBids = self.getRawBids(self.sc, self.bidsPath)
 
         # Collect the errors and save the result
         erroneousRecords = self.getErroneousRecords(rawBids)
-        erroneousRecords.saveAsTextFile(f"{outputBasePath}/{ERRONEOUS_DIR}")
+        erroneousRecords.saveAsTextFile(f"{self.outputBasePath}/{self.ERRONEOUS_DIR}")
 
         # Read the exchange rate information.
-        exchangeRates = self.getExchangeRates(sc, exchangeRatesPath)
+        exchangeRates = self.getExchangeRates(self.sc, self.exchangeRatesPath)
 
-        # Transform the rawBids and use the BidItem case class.
+        # Transform the rawBids and use the BidItem case class.u
         bids = self.getBids(rawBids, exchangeRates)
 
         # Load motels data.
-        motels = self.getMotels(sc, motelsPath)
+        motels = self.getMotels(self.sc, self.motelsPath)
 
         # Join the bids with motel names and utilize EnrichedItem case class.
         enriched = self.getEnriched(bids, motels)
-        enriched.saveAsTextFile(f"{outputBasePath}/{AGGREGATED_DIR}")
+        enriched.saveAsTextFile(f"{self.outputBasePath}/{self.AGGREGATED_DIR}")
 
-    def main(self, args):
-        bidsPath = args[0]
-        motelsPath = args[1]
-        exchangeRatesPath = args[2]
-        outputBasePath = args[3]
-        sc = SparkContext.getOrCreate(SparkConf())
-        spark = SparkSession(sc)
-        self.processData(sc, bidsPath, motelsPath, exchangeRatesPath, outputBasePath)
-
-
-
+MotelsHomeRecommendation('bids.txt', 'exchange_rate.txt', 'motels.txt', 'result.txt').processData()
